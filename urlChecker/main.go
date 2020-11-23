9561e1146a55d59d21fe3c4db4fbed3fbbb8b6fa
minkj1992 <minkj1992@gmail.com>
2020-11-23 17:58:03 +0900

    CH03: hitURL()

---
+++ urlChecker/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var (
+	errRequestFailed = errors.New("Request Failed")
+)
+
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		err := hitURL(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
+}
