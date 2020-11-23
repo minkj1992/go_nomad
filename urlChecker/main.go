b9303c8be41a5b76c140105d02b0251ba4ac45c0
minkj1992 <minkj1992@gmail.com>
2020-11-23 19:22:12 +0900

    CH03: print resp

--- urlChecker/main.go
+++ urlChecker/main.go
@@ -11,12 +11,12 @@ var (
 )
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.amazon.com/",
 		"https://www.reddit.com/",
-		"https://www.google.com/",
 		"https://soundcloud.com/",
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
@@ -24,10 +24,17 @@ func main() {
 	}
 
 	for _, url := range urls {
+		result := "OK"
 		err := hitURL(url)
+
 		if err != nil {
-			fmt.Println(err)
+			result = "FAILED"
 		}
+
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -35,6 +42,7 @@ func hitURL(url string) error {
 	fmt.Println("Checking: ", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
