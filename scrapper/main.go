ea9e81f302a7dfd725e38c86aee6b06552dc581f
minkj1992 <minkj1992@gmail.com>
2020-11-25 20:26:58 +0900

    CH04: extract job to struct

--- scrapper/main.go
+++ scrapper/main.go
@@ -5,17 +5,74 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 func main() {
+	var jobs []extractedJob
 	baseURL := "https://kr.indeed.com/jobs?q=go&limit=50"
 
 	totalPages := getPages(baseURL)
 	for i := 0; i < totalPages; i++ {
-		getPage(baseURL, i)
+		extractedJobs := getPage(baseURL, i)
+		jobs = append(jobs, extractedJobs...)
 	}
+
+	for _, job := range jobs {
+		fmt.Println(job)
+	}
+	// fmt.Println(jobs)
+}
+
+func getPage(baseURL string, page int) (jobs []extractedJob) {
+	pageUnit := 50
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*pageUnit)
+	fmt.Println("Requesting ", pageURL)
+
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := card.Find(".title>a").Text()
+	location := card.Find("sjcl").Text()
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages(url string) (pageCount int) {
@@ -35,12 +92,6 @@ func getPages(url string) (pageCount int) {
 	})
 
 	return
-}
-
-func getPage(baseURL string, page int) {
-	pageUnit := 50
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*pageUnit)
-	fmt.Println("Requesting ", pageURL)
 }
 
 func checkErr(err error) {
